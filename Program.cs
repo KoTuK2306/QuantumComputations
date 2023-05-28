0af7e29939c2e18139d546e6c2e3b08e106f6b2e
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2023-05-28 23:06:19 +0300

    реализована генерация строк степеней гамильтониана

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using System.Text;
 
 class Program
 {
@@ -8,21 +6,26 @@ class Program
 	static void Main()
 	{
 		Console.OutputEncoding = Encoding.UTF8;
-		Console.WriteLine("Введите коэффициент первого слагаемого гамильтониана");
+		Console.Write("Введите коэффициент первого слагаемого гамильтониана: ");
 		string a = Console.ReadLine()!;
-		Console.WriteLine("Введите индексы первого оператора Паули");
+		Console.Write("Введите индексы первого оператора Паули: ");
 		string sigma_1 = Console.ReadLine()!;
-		Console.WriteLine("Введите коэффициент второго слагаемого гамильтониана");
+		Console.Write("Введите коэффициент второго слагаемого гамильтониана: ");
 		string b = Console.ReadLine()!;
-		Console.WriteLine("Введите индексы второго оператора Паули");
+		Console.Write("Введите индексы второго оператора Паули: ");
 		string sigma_2 = Console.ReadLine()!;
-		Console.WriteLine("Введите коэффициент третьего слагаемого гамильтониана");
+		Console.Write("Введите коэффициент третьего слагаемого гамильтониана: ");
 		string c = Console.ReadLine()!;
-		Console.WriteLine("Введите индексы третьего оператора Паули");
+		Console.Write("Введите индексы третьего оператора Паули: ");
 		string sigma_3 = Console.ReadLine()!;
-		Console.WriteLine($"Введённый вами гамильтониан: {a}*{sigma}_{sigma_1} + {b}*{sigma}{sigma_2} + {c}*{sigma}{sigma_3}");
+		Console.WriteLine($"Введённый вами гамильтониан: H = {a}*{sigma}_{sigma_1} + {b}*{sigma}_{sigma_2} + {c}*{sigma}_{sigma_3}");
+		string[] sigma_array = Hamiltonian.countingOperators(sigma_1, sigma_2, sigma_3);
 
-		Console.WriteLine(Hamiltonian.countingOperators(sigma_1, sigma_2, sigma_3));
+		//sigma_12, sigma_23, sigma_123
+		Console.WriteLine($"{sigma}_AB = {sigma_array[0]}, {sigma}_BC = {sigma_array[1]}, {sigma}_ABC = {sigma_array[2]}");
+
+		Console.WriteLine($"H^2 = {Hamiltonian.calculateSecondDegreeOfHamiltonian(Convert.ToDouble(a), Convert.ToDouble(b), sigma_array)}");
+		Console.WriteLine($"H^3 = {Hamiltonian.calculateThirdDegreeOfHamiltonian(Convert.ToDouble(a), Convert.ToDouble(b), Convert.ToDouble(c), sigma_1, sigma_2,sigma_array)}");
 	}
 	class Calculation
 	{
@@ -92,17 +95,13 @@ class Program
 	}
 	class Hamiltonian
 	{
-		public static string countingOperators(string sigma_1, string sigma_2, string sigma_3)
+		public static string[] countingOperators(string sigma_1, string sigma_2, string sigma_3)
 		{
-			string sigma_13 = Calculation.Factors(sigma_1, sigma_3) + $"*{sigma}_" + Calculation.PauliMatrices(sigma_1, sigma_3);
 			string sigma_12 = Calculation.Factors(sigma_1, sigma_2) + $"*{sigma}_" + Calculation.PauliMatrices(sigma_1, sigma_2);
 			string sigma_23 = Calculation.Factors(sigma_2, sigma_3) + $"*{sigma}_" + Calculation.PauliMatrices(sigma_2, sigma_3);
 			string sigma_12_indexes = sigma_12.Split("_")[sigma_12.Split("_").Length - 1];
 			string sigma_123_draft = Calculation.Factors(sigma_12_indexes, sigma_3) + $"*{sigma}_" + Calculation.PauliMatrices(sigma_12_indexes, sigma_3);
 			string sigma_123;
-
-			Console.WriteLine($"sigma_12 = {sigma_12}");
-			Console.WriteLine($"sigma_123_draft = {sigma_123_draft}");
 
 			if (sigma_12.StartsWith("-1"))
 			{
@@ -165,7 +164,17 @@ class Program
 			{
 				sigma_123 = sigma_123_draft;
 			}
-			return $"{sigma}_AC = {sigma_13}, {sigma}_AB = {sigma_12}, {sigma}_BC = {sigma_23}, {sigma}_ABC = {sigma_123}";
+			string[] sigma_array = new string[] { sigma_12, sigma_23, sigma_123 };
+			return sigma_array;
+		}
+
+		public static string calculateSecondDegreeOfHamiltonian(double alpha, double beta, string[] arrayOfSigma)
+		{
+			return $"{sigma}_000 + {2 * alpha* beta}*{arrayOfSigma[0]}";
+		}
+		public static string calculateThirdDegreeOfHamiltonian(double alpha, double beta, double gamma, string sigma_1, string sigma_2, string[] arrayOfSigma)
+		{
+			return $"H + {2 * alpha * Math.Pow(beta, 2)}*{sigma_1}+{2 * Math.Pow(beta, 2) * beta}*{sigma_2} + {2 * alpha * beta * gamma}*{arrayOfSigma[2]}";
 		}
 	}
 }
